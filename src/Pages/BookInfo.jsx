564c01ea53f377aa671efc94d53a93fd4df364e6
Nicolas <nicolashorne99@gmail.com>
2025-01-19 17:04:35 -0500

    will only route /books/1

--- src/Pages/BookInfo.jsx
+++ src/Pages/BookInfo.jsx
@@ -1,24 +1,75 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Rating from "../components/ui/Rating";
+import Price from "../components/ui/Price";
 
 function BookInfo({ books }) {
   return (
     <div id="books__body">
-        <main id="books__main">
-            <div className="books__container">
-                <div className="row">
-                    <div className="book__selected--top">
-                        <Link to="/books" className="book__link">
-                            <FontAwesomeIcon icon="arrow-left" />
-                        </Link>
-                    </div>
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/books" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img
+                  src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg"
+                  alt=""
+                  className="book__selected--img"
+                />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">
+                  Crack the Coding Interview
+                </h2>
+                <Rating rating="4.5" />
+                <div className="book__selected--price">
+                  <Price originalPrice={50} salePrice={20} />
+                </div>
+                <div className="book__summary">
+                  <div className="book__summary--title">Summary</div>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Porro aliquid possimus nemo minima aspernatur obcaecati odio
+                    voluptatem! Doloribus, cum optio, esse minima molestiae,
+                    provident voluptatem laborum impedit sequi omnis veniam!
+                  </p>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Porro aliquid possimus nemo minima aspernatur obcaecati odio
+                    voluptatem! Doloribus, cum optio, esse minima molestiae,
+                    provident voluptatem laborum impedit sequi omnis veniam!
+                  </p>
+                  <button className="btn">
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="books__container">
+            <div className="row">
+                <div className="book__selected--top">
+                    <h2 className="book__selected--title--top">
+                        Recommended Books
+                    </h2>
                 </div>
             </div>
-        </main>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 // why isn't this working
 
-export default  BookInfo
+export default BookInfo;
